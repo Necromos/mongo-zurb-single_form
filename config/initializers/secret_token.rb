d5d04ab01b2dea1dfc6a674ad4b40faa2b69965c
Necromos <masternecromos@gmail.com>
2013-03-01 14:22:24 +0100

    initial-commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Necroworks::Application.config.secret_token = '277a3a54d2d81b95bc40f8119c6ec341db5e97fcb3e1f8b8f515d57a85a64ef4d5a74f522ca8b1d4cfff435eb5eeae3722fb1acd63ee5286d65f35ef7a3589bb'
